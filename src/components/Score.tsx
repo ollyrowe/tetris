60409bd364d18657cfd1a72e3db7161a34ea88fb
Olly Rowe <oliver.rowe101@gmail.com>
2023-09-03 15:23:31 +0100

    fix: adjust score section size on mobile

--- src/components/Score.tsx
+++ src/components/Score.tsx
@@ -1,29 +1,18 @@
 import React from "react";
 import { styled } from "styled-components";
 import Box from "./Box";
-import { ScreenSize, useScreenSize } from "../providers";
 
 interface Props {
   points: number;
 }
 
 const Score: React.FC<Props> = ({ points }) => {
-  const screenSize = useScreenSize();
-
-  return (
-    <StyledBox title="Score" size={screenSize}>
-      {points}
-    </StyledBox>
-  );
+  return <StyledBox title="Score">{points}</StyledBox>;
 };
 
 export default Score;
 
-interface StyledBoxProps {
-  size: ScreenSize;
-}
-
-const StyledBox = styled(Box)<StyledBoxProps>`
-  min-width: ${(props) => (props.size === "small" ? "90px" : "140px")};
+const StyledBox = styled(Box)`
+  min-width: 140px;
   text-align: right;
 `;
